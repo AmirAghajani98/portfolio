30c0378b615556f8c84f8cd160a9bc1855938d53
amir.aghajani1377@gmail.com <amir.aghajani1377@gmail.com>
2024-01-03 13:25:14 +0330

    feat-font

---
+++ src/app/font/trotes/LICENSE.md
@@ -0,0 +1,5 @@
+Trotes font is free for personal use only.
+
+This font was created and distributed by FG Studios.
+
+You can purchase the full font and commercial license from https://www.fgstudios.net/trotes-pro-font/
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Amir Mohammad Aghajnai",
-  description: "web developer ",
+  description: "web developer",
 };
 
 export default function RootLayout({
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
