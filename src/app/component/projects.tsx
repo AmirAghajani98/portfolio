fc79b5b11a4104ba603788b801bd8629d3abece4
amir.aghajani1377@gmail.com <amir.aghajani1377@gmail.com>
2024-08-07 13:11:01 +0330

    remove-slider

--- src/app/component/projects.tsx
+++ src/app/component/projects.tsx
@@ -2,9 +2,6 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const Projects: React.FC = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -24,37 +21,6 @@ const Projects: React.FC = () => {
     if (event.target === event.currentTarget) {
       closeModal();
     }
-  };
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 700,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
   };
 
   return (
