a1e163b4f29c84928a56a924c7040df855705adb
amir.aghajani1377@gmail.com <amir.aghajani1377@gmail.com>
2024-08-07 13:24:48 +0330

    fix-intro-margins

--- src/app/component/Intro.tsx
+++ src/app/component/Intro.tsx
@@ -20,7 +20,7 @@ const Intro: React.FC = () => {
             <br />
             Amir Mohammad Aghajani
           </h1>
-          <p className="my-20 text-3xl sm:text-4xl">
+          <p className="my-20 text-3xl sm:text-4xl text-center">
             <span className=""></span>
             <strong className="font-mono dark:text-slate-100 text-[#121a23]">
               <ReactTyped
@@ -37,15 +37,15 @@ const Intro: React.FC = () => {
             </strong>
           </p>
           <div className="sm:mt-40 mt-20 dark:text-slate-100 text-[#121a23]">
-            <h2 className="justify-center text-3xl font-bold sm:flex font-sans">
+            <h2 className="justify-center sm:text-3xl text-2xl font-semibold sm:flex font-sans">
               Connect with me
             </h2>
           </div>
-          <div className="mt-10 justify-center mr-4 flex">
+          <div className="mt-10 justify-center flex w-full sm:mx-auto">
             <a
               href="mailto:amir.aghajani1377@gmail.com"
               target={"_blank"}
-              className="mx-3"
+              className="sm:mx-3"
             >
               <Image
                 src="./img/2.png"
@@ -58,7 +58,7 @@ const Intro: React.FC = () => {
             <a
               href="https://github.com/AmirAghajani98"
               target={"_blank"}
-              className="mx-3"
+              className="sm:mx-3"
             >
               <Image
                 src="./img/7.png"
@@ -68,7 +68,7 @@ const Intro: React.FC = () => {
                 className="mx-4 my-2 p-2 bg-slate-700 bg-opacity-30 dark:bg-slate-100 dark:bg-opacity-70 rounded-2xl hover:bg-opacity-40 dark:hover:bg-opacity-50 shadow-inner shadow-slate-500 border-y border-slate-400 dark:shadow-slate-500"
               />
             </a>
-            <a href="https://t.me/Amr431" target={"_blank"} className="mx-3">
+            <a href="https://t.me/Amr431" target={"_blank"} className="sm:mx-3">
               <Image
                 src="./img/4.png"
                 width={50}
@@ -80,7 +80,7 @@ const Intro: React.FC = () => {
             <a
               href="https://www.linkedin.com/in/amir-aghajani/"
               target={"_blank"}
-              className="mx-3"
+              className="sm:mx-3"
             >
               <Image
                 src="./img/5.png"
