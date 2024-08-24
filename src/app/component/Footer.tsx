f91ffe31c02f492b4721ab3390af83d7b3798485
amir.aghajani1377@gmail.com <amir.aghajani1377@gmail.com>
2024-08-24 13:28:51 +0330

    feat-new-copyright

--- src/app/component/Footer.tsx
+++ src/app/component/Footer.tsx
@@ -3,8 +3,8 @@
 export default function Footer() {
   return (
     <div className="z-10 opacity-95 py-auto h-14 dark:bg-slate-700 bg-slate-500 w-full shadow-sm shadow-slate-700 flex justify-center items-center">
-      <p className="text-center m-auto text-lg font-serif font-semibold  dark:text-slate-200 text-slate-900">
-        Powered by Amir Aghajani
+      <p className="text-center m-auto text-lg font-sans font-semibold  dark:text-slate-200 text-slate-900">
+        Built with "♥"
       </p>
     </div>
   );
