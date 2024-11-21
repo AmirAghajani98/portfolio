2e831c0ebd9a2e7285c40d7fb168c9eac5045063
amir <amir.aghajani1377@gmail.com>
2024-11-21 11:28:18 +0330

    add-lang

---
+++ src/app/component/language.ts
@@ -0,0 +1,18 @@
+const languageData = {
+  en: {
+    welcomeMessage: "Welcome to our application!",
+    login: "Login",
+    signUp: "Sign Up",
+    aboutUs: "About Us",
+    contact: "Contact",
+  },
+  fa: {
+    welcomeMessage: "به برنامه ما خوش آمدید!",
+    login: "ورود",
+    signUp: "ثبت نام",
+    aboutUs: "درباره ما",
+    contact: "تماس",
+  },
+};
+
+export default languageData;
