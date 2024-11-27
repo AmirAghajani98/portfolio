b5a9a282f64f52a9c8eee4611696fc89941138f5
amir <amir.aghajani1377@gmail.com>
2024-11-27 23:58:41 +0330

    fix-resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,17 @@
 "use client";
+
 import "./globals.css";
 import "./i18";
 import ProviderTheme from "./component/ProviderTheme";
 import { Suspense } from "react";
-import { useTranslation } from "react-i18next";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { i18n } = useTranslation();
-
-  const currentLocale = i18n.language;
-
   return (
-    <html
-      lang={currentLocale}
-      dir={currentLocale === "fa" ? "rtl" : "ltr"}
-      className="scroll-smooth"
-    >
+    <html className="scroll-smooth">
       <body>
         <Suspense fallback={<div>Loading...</div>}>
           <ProviderTheme>
