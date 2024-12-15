8809c0a96778f4c803ddfd69a83f38fb5cd16dfd
amir <amir.aghajani1377@gmail.com>
2024-12-15 22:19:24 +0330

    add-site-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     siteName: "amir-aghajani-portfolio",
     images: [
       {
-        url: "/img/aaa.jpg",
+        url: "/img/site.png",
         width: 800,
         height: 600,
         alt: "Amir Aghajani Portfolio Image",
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     title: "Amir Aghajani - Web Developer",
     description:
       "Software engineer and web developer specializing in modern web technologies.",
-    images: ["/img/aaa.jpg"],
+    images: ["/img/site.png"],
   },
   icons: {
     icon: "/favicon.ico",
