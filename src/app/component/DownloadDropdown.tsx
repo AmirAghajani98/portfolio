a1cd03c11d817b6d189cd04295caabc75cce7006
amir <amir.aghajani1377@gmail.com>
2025-01-25 13:28:59 +0330

    fix-resume

--- src/app/component/DownloadDropdown.tsx
+++ src/app/component/DownloadDropdown.tsx
@@ -11,8 +11,8 @@ const DownloadDropdown = () => {
 
   return (
     <div className="relative mx-auto flex w-full max-w-lg items-center justify-center">
-      <div className="relative z-50 flex w-full cursor-pointer items-center overflow-hidden rounded-xl border border-slate-500 dark:border-slate-800 sm:p-[1.5px]">
-        <div className="relative z-50 flex w-full rounded-[0.60rem] dark:bg-gray-900 bg-gray-700 dark:hover:bg-gray-950 hover:bg-gray-800">
+      <div className="relative sm:z-50 flex w-full cursor-pointer items-center overflow-hidden rounded-xl border border-slate-500 dark:border-slate-800 sm:p-[1.5px]">
+        <div className="relative sm:z-50 flex w-full rounded-[0.60rem] dark:bg-gray-900 bg-gray-700 dark:hover:bg-gray-950 hover:bg-gray-800">
           <Link
             onClick={toggleDropdown}
             className="hidden sm:flex w-full justify-between font-mono items-center gap-x-3 text-slate-100 dark:text-slate-200 font-medium rounded-lg px-3 py-2 z-5 transition duration-200 border-4 border-transparent bg-gradient-to-l from-gradient-end via-gradient-middle to-gradient-start animate-gradientBorder"
@@ -53,7 +53,7 @@ const DownloadDropdown = () => {
           rel="noopener noreferrer"
           target="_blank"
           download="resume-en.pdf"
-          className="p-2.5 sm:mr-1 mt-1 border-b sm:border-none border-slate-700 font-mono cursor-pointer shadow dark:bg-gray-900 bg-gray-700 dark:hover:bg-gray-950 hover:bg-gray-800 sm:rounded-lg rounded-t-lg"
+          className="p-2.5 sm:mr-1 sm:mt-0 mt-1 border-b sm:border-none border-slate-500 dark:border-b dark:border-slate-700 font-mono cursor-pointer shadow dark:bg-gray-900 bg-gray-700 dark:hover:bg-gray-950 hover:bg-gray-800 sm:rounded-lg rounded-t-lg"
         >
           English
         </Link>
