9b3f033bff977f75df9d7b992a3944b0f66c8524
amir <amir.aghajani1377@gmail.com>
2025-03-10 00:40:44 +0330

    fix-forms

---
+++ mail-config.ts
@@ -0,0 +1,10 @@
+export const emailConfig = {
+  serviceID: "service_17pm2lq",
+  templateID: "template_8eyihgv",
+  publicKey: "E_RctwYO87SRR-0sj",
+};
+export const contactConfig = {
+  serviceID: "service_17pm2lq",
+  templateID: "template_498q42k",
+  publicKey: "E_RctwYO87SRR-0sj",
+};
